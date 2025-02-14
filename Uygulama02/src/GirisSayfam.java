0972caeb0630a887407ee070f7be8f926f93072f
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-14 18:12:55 +0300

    panel label olusturuldu

--- Uygulama02/src/GirisSayfam.java
+++ Uygulama02/src/GirisSayfam.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class GirisSayfam {
      JFrame frame;
-     JTextField kullanıcıAdı;
+     JTextField kullanıcıAdı,sifre;
      public GirisSayfam(){
          JPanel panelUst = new JPanel();
          panelUst.setLayout(new FlowLayout());
@@ -11,14 +11,35 @@ public class GirisSayfam {
          labelUst.setFont(new Font("Arial",Font.PLAIN,20));
 
          kullanıcıAdı = new JTextField();
-         kullanıcıAdı.setPreferredSize(new Dimension(200,40));
+         kullanıcıAdı.setPreferredSize(new Dimension(300,60));
 
          panelUst.add(labelUst);
          panelUst.add(kullanıcıAdı);
 
+
+         JPanel panelAlt = new JPanel();
+         panelAlt.setLayout(new FlowLayout());
+         JLabel labelAlt = new JLabel("Şifre :");
+         labelAlt.setFont(new Font("Arial",Font.PLAIN,20));
+
+         sifre = new JTextField();
+         sifre.setPreferredSize(new Dimension(300,60));
+
+         panelAlt.add(labelAlt);
+         panelAlt.add(sifre);
+
+
+         frame = new JFrame();
+
          frame.setTitle("Kullanıcı Girişi");
-         frame.setSize(300, 200);
+         frame.setSize(400, 300);
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.setLocationRelativeTo(null);
+         frame.setLayout(new GridLayout(3,1));
+
+         frame.add(panelUst);
+         frame.add(panelAlt);
+
+
+         frame.setVisible(true);
      }
 }
