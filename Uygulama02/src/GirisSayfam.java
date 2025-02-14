354efe2745509518adf265e84fcbd6d11485aa10
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-14 18:00:05 +0300

    Class olusturuldu

---
+++ Uygulama02/src/GirisSayfam.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GirisSayfam {
+     JFrame frame;
+     public GirisSayfam(){
+         frame.setTitle("Kullanıcı Girişi");
+         frame.setSize(300, 200);
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setLocationRelativeTo(null);
+     }
+}
