f59211e86eed250ce65e588dce800122eba08da7
Mir4cshn <159525962+Mir4cshn@users.noreply.github.com>
2025-02-14 18:05:43 +0300

    panel label olusturuldu

--- Uygulama02/src/GirisSayfam.java
+++ Uygulama02/src/GirisSayfam.java
@@ -1,8 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GirisSayfam {
      JFrame frame;
+     JTextField kullanıcıAdı;
      public GirisSayfam(){
+         JPanel panelUst = new JPanel();
+         panelUst.setLayout(new FlowLayout());
+         JLabel labelUst = new JLabel("Kullanıcı Adı :");
+         labelUst.setFont(new Font("Arial",Font.PLAIN,20));
+
+         kullanıcıAdı = new JTextField();
+         kullanıcıAdı.setPreferredSize(new Dimension(200,40));
+
+         panelUst.add(labelUst);
+         panelUst.add(kullanıcıAdı);
+
          frame.setTitle("Kullanıcı Girişi");
          frame.setSize(300, 200);
          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
